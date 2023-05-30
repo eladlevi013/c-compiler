e7014eda8c74c4d76f4e96d09fe09d65533ccb6c
Itamarcohen2001 <Itamarcohen2001@gmail.com>
2023-05-30 17:11:44 +0300

    start to fail

--- Project/part3_3ac/tac_helpers.c
+++ Project/part3_3ac/tac_helpers.c
@@ -1,8 +1,14 @@
+#include <stddef.h>
+#include <ctype.h>
 
-// Part2 - 3AC
+// Part3 - 3AC
 void addVar(node* node, char* var);
 void freshVar(node* node);
 char* freshLabel();
+void addCode(node* node, char* code);
+
+int var = 0;
+int label = 1;
 
 // void tac_gen(node* root)
 // {
@@ -24,3 +30,32 @@ char* freshLabel(){
 	char* L = strdup(new);
 	return L;
 }
+
+void addCode(node* node, char* code){
+	char buffer[10000] = "";
+	if (!strcmp(node->token, "MAIN")){
+		sprintf(buffer, "%s", "main:\n\tBeginFunc\n");
+		node->code = strdup(buffer);
+	}
+
+	if (!strcmp(node->token, "FUNCTION")){
+		sprintf(buffer, "%s:\n%s", node->nodes[0]->token,"\tBeginFunc\n");
+		node->code = strdup(buffer);
+	}
+
+	if (node->count > 0){
+		for (int i = 0; i< node->count; i++){
+			if (strcmp(node->nodes[i]->code,"")){
+				sprintf(buffer + strlen(buffer), "%s", node->nodes[i]->code);
+			}
+		}
+	}
+	sprintf(buffer + strlen(buffer), "%s", code);
+	node->code = strdup(buffer);
+
+	if (!strcmp(node->token, "MAIN") || !strcmp(node->token, "FUNCTION")){
+		sprintf(buffer + strlen(buffer), "\t%s", "EndFunc\n\n");
+		node->code = strdup(buffer);
+	}
+}
+
