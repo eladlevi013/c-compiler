b3d62ebb16244993f1e94843a0a72ff9ded31178
Itamarcohen2001 <Itamarcohen2001@gmail.com>
2023-05-30 16:51:11 +0300

    3ac add label and var

--- Project/part3_3ac/tac_helpers.c
+++ Project/part3_3ac/tac_helpers.c
@@ -1,4 +1,26 @@
-void tac_gen(node* root)
-{
-    printf("tac_gen\n");
+
+// Part2 - 3AC
+void addVar(node* node, char* var);
+void freshVar(node* node);
+char* freshLabel();
+
+// void tac_gen(node* root)
+// {
+//     printf("tac_gen\n");
+// }
+void addVar(node* node, char* var){
+	node->var = strdup(var);
+}
+
+void freshVar(node* node){
+	char new[10];
+	sprintf(new ,"t%d", var++);
+	node->var = strdup(new);
+}
+
+char* freshLabel(){
+	char new[10] = "";
+	sprintf(new ,"L%d", label++);
+	char* L = strdup(new);
+	return L;
 }
